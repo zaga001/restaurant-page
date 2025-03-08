81a55fe8ec906b029a84a32fbe534345db384081
zaga <163298261+zaga001@users.noreply.github.com>
2025-03-08 18:57:59 +0700

    Add tab switching

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import "./styles.css"
-import { pageLoad } from "./page-load.js"
+import { loadHome } from "./home.js"
+import { loadMenu } from "./menu.js"
+import { loadAbout } from "./about.js"
 
-pageLoad()
+loadHome()
+
+const home = document.querySelector("#home")
+const menu = document.querySelector("#menu")
+const about = document.querySelector("#about")
+const buttons = document.querySelectorAll("button")
+const content = document.querySelector("#content")
+
+buttons.forEach(element => {
+    element.addEventListener("click", () => {
+        content.innerHTML = ""
+    })
+});
+
+home.addEventListener("click", loadHome)
+menu.addEventListener("click", loadMenu)
+about.addEventListener("click", loadAbout)
